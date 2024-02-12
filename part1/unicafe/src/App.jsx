b9a5b3bed87e632dc019447f4b270ada632511af
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-02-12 10:19:57 -0800

    Completed 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,31 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
+
+const Header = ({ text }) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
+};
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <>
+      <button onClick={handleClick}>{text}</button>
+    </>
+  );
+};
+
+const StatisticsOutput = ({ text, state }) => {
+  return (
+    <>
+      <p>
+        {text} {state}
+      </p>
+    </>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,7 +33,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <Header text='Give Feedback' />
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <Header text='statistics' />
+      <StatisticsOutput text='good' state={good} />
+      <StatisticsOutput text='neutral' state={neutral} />
+      <StatisticsOutput text='bad' state={bad} />
+    </div>
+  );
 };
 
 export default App;
