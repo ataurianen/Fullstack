5ce5036faedbe3287158bac0fe86a7e96f34e20f
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-02-12 10:53:31 -0800

    Completed 1.7-1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,22 +9,33 @@ const Header = ({ text }) => {
   );
 };
 
-const Button = ({ handleClick, text }) => {
+const Button = ({ onClick, text }) => {
   return (
     <>
-      <button onClick={handleClick}>{text}</button>
+      <button onClick={onClick}>{text}</button>
     </>
   );
 };
 
-const StatisticsOutput = ({ text, state }) => {
-  return (
-    <>
-      <p>
-        {text} {state}
-      </p>
-    </>
-  );
+const Statistics = (props) => {
+  if (props.total === 0) {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>good: {props.good}</p>
+        <p>neutral: {props.neutral}</p>
+        <p>bad: {props.bad}</p>
+        <p>total: {props.total}</p>
+        <p>average: {props.average}</p>
+        <p>positive: {props.good / props.total}</p>
+      </>
+    );
+  }
 };
 
 const App = () => {
@@ -32,29 +43,41 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setAverage(average + 1);
+    setTotal(total + 1);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setAverage(average + 0);
+    setTotal(total + 1);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
+    setAverage(average - 1);
+    setTotal(total + 1);
   };
 
   return (
     <div>
       <Header text='Give Feedback' />
-      <Button handleClick={handleGoodClick} text='good' />
-      <Button handleClick={handleNeutralClick} text='neutral' />
-      <Button handleClick={handleBadClick} text='bad' />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
       <Header text='statistics' />
-      <StatisticsOutput text='good' state={good} />
-      <StatisticsOutput text='neutral' state={neutral} />
-      <StatisticsOutput text='bad' state={bad} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+      />
     </div>
   );
 };
