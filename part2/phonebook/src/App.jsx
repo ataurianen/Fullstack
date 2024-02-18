171e2b1e209b3a7a7e8dcd9d6683685f660026b0
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-02-18 13:39:56 -0800

    Completed 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,56 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
+
+const Header = ({ text }) => {
+  return <h2>{text}</h2>;
+};
+
+const Person = ({ name }) => {
+  return <p>{name}</p>;
+};
+
+const People = ({ persons }) => {
+  return (
+    <div>
+      {persons.map((person) => (
+        <Person key={person.name} name={person.name} />
+      ))}
+    </div>
+  );
+};
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const addPerson = (e) => {
+    e.preventDefault();
+
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
+
+  const handleInputChange = (e) => {
+    setNewName(e.target.value);
+  };
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
+      <Header text={'Phonebook'} />
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          Name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <Header text={'Numbers'} />
+      <People persons={persons} />
     </div>
   );
 };
