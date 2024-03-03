0745262b1cfe6eb43e47becfd6b638aea702f51a
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-03-03 13:14:03 -0800

    Completed 4.4

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  } else if (blogs.length === 1) {
+    return blogs[0].likes;
+  } else {
+    return blogs.reduce((sum, currentValue) => sum + currentValue.likes, 0);
+  }
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
