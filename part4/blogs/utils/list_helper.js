2077b3fa6ff1259a03bdb9680551a30190d8a90c
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-03-04 10:25:26 -0800

    Completed 4.5

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -1,18 +1,41 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1;
 };
 
 const totalLikes = (blogs) => {
   if (blogs.length === 0) {
-    return 0;
-  } else if (blogs.length === 1) {
-    return blogs[0].likes;
+    return 'List is empty';
   } else {
     return blogs.reduce((sum, currentValue) => sum + currentValue.likes, 0);
+  }
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 'List is empty';
+  } else if (blogs.length === 1) {
+    return {
+      title: blogs[0].title,
+      author: blogs[0].author,
+      likes: blogs[0].likes,
+    };
+  } else {
+    let mostLikedBlog = blogs[0];
+    blogs.forEach((blog) => {
+      if (blog.likes > mostLikedBlog.likes) {
+        mostLikedBlog = blog;
+      }
+    });
+    return {
+      title: mostLikedBlog.title,
+      author: mostLikedBlog.author,
+      likes: mostLikedBlog.likes,
+    };
   }
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
