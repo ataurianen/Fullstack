bf3b11965040f3e63af2463268d9da59b954e157
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-03-12 11:11:19 -0700

    Completed 4.17

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -1,8 +1,13 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   response.json(blogs);
 });
 
@@ -17,6 +22,10 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
+
+  const user = await User.findById(body.userId);
+  console.log(user);
+
   if (!body.title || !body.url) {
     response.status(400).end();
   } else {
@@ -25,9 +34,13 @@ blogsRouter.post('/', async (request, response) => {
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
+      user: user.id,
     });
 
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     response.status(201).json(savedBlog);
   }
 });
