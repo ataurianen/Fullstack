e1665e97a4e3d28ce56c8165de8d4efd95bee0e5
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-03-13 11:44:58 -0700

    Completed 5.1

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,24 +1,69 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
-  return (
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (e) {
+      console.log('Wrong credentials');
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        Username:
+        <input
+          type='text'
+          value={username}
+          name='Username'
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        Password:
+        <input
+          type='password'
+          value={password}
+          name='password'
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type='submit'>Login</button>
+    </form>
+  );
+
+  const blogDisplay = () => (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
+      <h2>Blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
 
-export default App
+  return <div>{user === null ? loginForm() : blogDisplay()}</div>;
+};
+
+export default App;
