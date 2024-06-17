6faa5985af0789723d062dc887bf45d1a11b2ff5
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-06-17 13:56:15 -0700

    Updated to use ReactRouter

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from "react";
-import Authors from "./components/Authors";
-import Books from "./components/Books";
-import NewBook from "./components/NewBook";
+import { Routes, Route, Link } from 'react-router-dom';
+import Authors from './components/Authors';
+import Books from './components/Books';
+import NewBook from './components/NewBook';
 
 const App = () => {
-  const [page, setPage] = useState("authors");
-
   return (
     <div>
       <div>
-        <button onClick={() => setPage("authors")}>authors</button>
-        <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        <Link to='/authors'>authors</Link>
+        {'  '}
+        <Link to='/books'>books</Link>
+        {'  '}
+        <Link to='/add'>add book</Link>
       </div>
-
-      <Authors show={page === "authors"} />
-
-      <Books show={page === "books"} />
-
-      <NewBook show={page === "add"} />
+      <div>
+        <Routes>
+          <Route path='/authors' element={<Authors />} />
+          <Route path='/books' element={<Books />} />
+          <Route path='/add' element={<NewBook />} />
+        </Routes>
+      </div>
     </div>
   );
 };
